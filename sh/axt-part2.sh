f5d2849b811af3a118eadc2867d9b82daa48b81c
ShanStone <31815718+ShanStone@users.noreply.github.com>
2026-01-29 15:39:46 +0800

    fix fancontrol default

--- sh/axt-part2.sh
+++ sh/axt-part2.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+FAN_PATH="./feeds/fancontrol"
+FAN_JS="$FAN_PATH/luci-app-fancontrol/htdocs/luci-static/resources/view/fancontrol.js"
+[ ! -f "$FAN_JS" ] && FAN_JS=$(find $FAN_PATH -name "fancontrol.js")
+if [ -f "$FAN_JS" ]; then
+    sed -i 's/read_direct/trimmed/g' "$FAN_JS"
+fi
+
 sed -i 's/192.168.1.1/10.10.0.1/g' ./package/base-files/files/bin/config_generate
 mkdir ./package/custom
 git clone https://github.com/sbwml/autocore-arm.git ./package/custom/
